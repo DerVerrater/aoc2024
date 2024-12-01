471e31a6ff052e5ad4ba2ba75a3c64b44d0ab8d1
Robert Garrett <robertgarrett404@gmail.com>
2024-12-01 16:03:47 -0600

    Day 1 Part 2 complete!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 mod input_constants;
 
-use day1_part1::process_d1p1;
+use day1_part1::{process_d1p1, process_d1p2};
 
 fn main() {
-    println!("Day 1 result: {}", process_d1p1(input_constants::DAY1));
+    println!(
+        "Day 1 Part 1 result: {}",
+        process_d1p1(input_constants::DAY1)
+    );
+    println!(
+        "Day 1 Part 2 result: {}",
+        process_d1p2(input_constants::DAY1)
+    );
 }
 
 mod day1_part1 {
+    use std::collections::HashMap;
 
-    pub fn process_d1p1(input: &str) -> i32 {
-        let (mut col1, mut col2): (Vec<_>, Vec<_>) = input
+    fn parse_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
+        input
             .split("\n")
             .map(|line| -> (i32, i32) {
                 let mut parts = line.split("   ");
@@ -26,7 +34,11 @@ mod day1_part1 {
 
                 return (first_int, second_int);
             })
-            .unzip();
+            .unzip()
+    }
+
+    pub fn process_d1p1(input: &str) -> i32 {
+        let (mut col1, mut col2): (Vec<_>, Vec<_>) = parse_lists(input);
 
         col1.sort();
         col2.sort();
@@ -37,6 +49,28 @@ mod day1_part1 {
             .map(|(left, right)| (left - right).abs())
             .sum();
         return element_diffs;
+    }
+
+    pub fn process_d1p2(input: &str) -> i32 {
+        let (col1, col2): (Vec<_>, Vec<_>) = parse_lists(input);
+
+        let frequencies = col2.iter().copied().fold(HashMap::new(), |mut map, val| {
+            map.entry(val).and_modify(|frq| *frq += 1).or_insert(1);
+            map
+        });
+
+        let sum: i32 = col1
+            .into_iter()
+            .map(|value| {
+                if let Some(freq) = frequencies.get(&value) {
+                    return value * freq;
+                } else {
+                    return 0;
+                }
+            })
+            .sum();
+
+        return sum;
     }
 
     #[cfg(test)]
@@ -51,8 +85,15 @@ mod day1_part1 {
 3   3";
 
         #[test]
-        fn run_example() {
+        fn run_part1_example() {
             assert_eq!(process_d1p1(SAMPLE_TEXT), 11);
+        }
+
+        #[test]
+        fn run_part2_example() {
+            let result = process_d1p2(SAMPLE_TEXT);
+            eprintln!("Got result: {result}");
+            assert_eq!(result, 31);
         }
     }
 }
