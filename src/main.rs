16f234e3c869dfb82d7c1cd3f1fa95aa01c23e01
Robert Garrett <robertgarrett404@gmail.com>
2024-12-02 13:59:58 -0600

    Day 2 Part 1 -- example passes test

--- src/main.rs
+++ src/main.rs
@@ -97,3 +97,102 @@ mod day1 {
         }
     }
 }
+
+mod day2 {
+
+    type Record = [i32; 5];
+
+    fn parse_reports(input: &str) -> impl Iterator<Item = Record> + use<'_> {
+        input.split("\n").map(|line| -> Record {
+            let parts = line.split(" ");
+            let mut num_iter =
+                parts.map(|number| number.parse::<i32>().expect("Failed to parse a number"));
+            const ERRMSG: &str = "Ran out of digits while collecting report";
+            return [
+                num_iter.next().expect(ERRMSG),
+                num_iter.next().expect(ERRMSG),
+                num_iter.next().expect(ERRMSG),
+                num_iter.next().expect(ERRMSG),
+                num_iter.next().expect(ERRMSG),
+            ];
+        })
+    }
+
+    #[derive(PartialEq)]
+    enum SafetyGrade {
+        Safe,       // report is safe. Count this one!
+        TooSlow,    // report changes by < 1
+        TooFast,    // report changes by > 3
+        Maxima,     // report was going up, and is now going down
+        Minima,     // report was going down, and is now going up
+    }
+
+    fn grade_record(record: Record) -> SafetyGrade {
+        let mut increasing = false;
+        let mut decreasing = false;
+
+        for idx in 0..(record.len()-2) {
+            let first = record.get(idx).unwrap();
+            let second = record.get(idx+1).unwrap();
+            let slope = second - first;
+            
+            if slope == 0 {
+                // There was no change. Fail TooSlow
+                return SafetyGrade::TooSlow
+            } else if slope > 0 {
+                // slope increasing. Check if local minima, then slope magnitude
+                increasing = true;
+                if decreasing {
+                    // if decreasing was set before, this is a (local) minima. Fail
+                    return SafetyGrade::Minima;
+                } else if slope > 3{
+                    // if rising faster than 3, emit failure
+                    return SafetyGrade::TooFast;
+                }
+            } else if slope < 0 {
+                // slope decreasing. Check if local maxima, then slope magnitude
+                decreasing = false;
+                if increasing {
+                    // if increasing was set before, this is a (local) maxima. Fail
+                    return SafetyGrade::Maxima;
+                } else if slope < -3 {
+                    // if dropping faster than -3, emit failure
+                    return SafetyGrade::TooFast;
+                }
+            }
+        }
+        
+        // no unsafe values were detected. Must be safe
+        return SafetyGrade::Safe
+    }
+
+    pub fn process_d2p1(input: &str) -> i32 {
+        parse_reports(input)
+        .map(grade_record)
+        .fold(0, |count, grade| {
+            if grade == SafetyGrade::Safe {
+                count + 1
+            } else {
+                count
+            }
+        })
+    }
+
+    #[cfg(test)]
+    mod day2_tests {
+        use super::*;
+
+        const SAMPLE_TEXT: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        #[test]
+        fn run_part1_example() {
+            let result = process_d2p1(SAMPLE_TEXT);
+            eprintln!("Got result: {result}");
+            assert_eq!(result, 2);
+        }
+    }
+}
