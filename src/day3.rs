896de3260f9a4de9ce41e520f7e7a154a4312052
Robert Garrett <robertgarrett404@gmail.com>
2024-12-03 18:38:11 -0600

    Instruction parser includes mul tag
    If the "mul(" tag is found as part of the instruction parser, then I
    need fewer levels in the d3p1 driving function. This seems basically
    required to make the parser alternation not repetetive for Part 2.

--- src/day3.rs
+++ src/day3.rs
@@ -2,16 +2,12 @@ pub fn process_d3p1(input: &str) -> i32 {
     let mut sum: i32 = 0;
     let mut scan_location = input;
     loop {
-        if let Ok(keyword_start) = try_find_mul(scan_location) {
+        if let Ok((remainder, value)) = try_consume_instruction(scan_location) {
             // parsing has succeeded in finding a mul(x,y) instruction!
-            if let Ok((remainder, value)) = try_consume_instruction(keyword_start) {
-                // 1: Advance scan location forward to the remaining text
-                scan_location = remainder;
-                // 2: Multiply & accumulate the numbers
-                sum += value;
-            } else {
-                scan_location = &scan_location[1..];
-            }
+            // 1: Advance scan location forward to the remaining text
+            scan_location = remainder;
+            // 2: Multiply & accumulate the numbers
+            sum += value;
         } else {
             // if no "mul" keyword was found, advance pointer
             // guard against reaching the end of the pointer.
@@ -54,15 +50,17 @@ fn try_find_mul(input: &str) -> Result<&str, ParseError> {
 }
 
 fn try_consume_instruction(input: &str) -> Result<(&str, i32), ParseError> {
+    // Match the "mul(" tag
+    let remainder = try_find_mul(input)?;
     // Get remaining input and digits, or else advance slice by 1 and try again.
-    let (remainder, first_digits) = try_collect_digits(input)?;
+    let (remainder, first_digits) = try_collect_digits(remainder)?;
     // consume the following comma, or else advance slice
     let remainder = try_consume_comma(remainder)?;
     // get the *next* input digits, or else advance slice
     let (remainder, second_digits) = try_collect_digits(remainder)?;
     // get the closing parenthesis, or else advance slice
     let remainder = try_consume_closeparen(remainder)?;
-    
+
     Ok((&remainder, first_digits * second_digits))
 }
 
